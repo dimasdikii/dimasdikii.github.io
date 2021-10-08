ef40c74a0280b90c958720a59dc22b6499b91351
Dimas <aizuwan.id@gmail.com>
2021-10-08 12:47:35 +0700

    fix error

--- src/containers/pages/App/index.js
+++ src/containers/pages/App/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Link, Switch } from "react-router-dom";
 import Home from '../Home';
 import BlogDetail from "../BlogDetail";
 import Shop from "../Shop";
@@ -13,19 +14,34 @@ import NotFound from '../NotFound';
 class App extends Component {
   render() {
     return (
-        <Router>
-            <Switch>
-              <Route exact path={process.env.PUBLIC_URL + '/'} component={Home} />
-              <Route path={process.env.PUBLIC_URL + '/post/:postID'} component={BlogDetail} />
-              <Route exact path={process.env.PUBLIC_URL + '/shop'} component={Shop} />
-              <Route path={process.env.PUBLIC_URL + '/product/:productID'} component={ProductDetail} />
-              <Route path={process.env.PUBLIC_URL + '/order/:productID'} component={Order} />
-              <Route path={process.env.PUBLIC_URL + '/checkout/:orderID'} component={Checkout} />
-              <Route path={process.env.PUBLIC_URL + '/thanks/:orderID'} component={Thanks} />
-              <Route path={process.env.PUBLIC_URL + '/paynotif/:orderID'} component={PayNotif} />
+        // <Router>
+        //     <Switch>
+        //       <Route exact path={process.env.PUBLIC_URL + '/'} component={Home} />
+        //       <Route path={process.env.PUBLIC_URL + '/post/:postID'} component={BlogDetail} />
+        //       <Route exact path={process.env.PUBLIC_URL + '/shop'} component={Shop} />
+        //       <Route path={process.env.PUBLIC_URL + '/product/:productID'} component={ProductDetail} />
+        //       <Route path={process.env.PUBLIC_URL + '/order/:productID'} component={Order} />
+        //       <Route path={process.env.PUBLIC_URL + '/checkout/:orderID'} component={Checkout} />
+        //       <Route path={process.env.PUBLIC_URL + '/thanks/:orderID'} component={Thanks} />
+        //       <Route path={process.env.PUBLIC_URL + '/paynotif/:orderID'} component={PayNotif} />
+        //       <Route component={NotFound} />
+        //     </Switch>
+        // </Router>
+        <HashRouter basename={process.env.PUBLIC_URL}>
+          <Route render = {({ location }) => (
+            <Switch location = { location }>
+              <Route exact path='/' component={Home} />
+              <Route path='/post/:postID' component={BlogDetail} />
+              <Route exact path='/shop' component={Shop} />
+              <Route path='/product/:productID' component={ProductDetail} />
+              <Route path='/order/:productID' component={Order} />
+              <Route path='/checkout/:orderID' component={Checkout} />
+              <Route path='/thanks/:orderID' component={Thanks} />
+              <Route path='/paynotif/:orderID' component={PayNotif} />
               <Route component={NotFound} />
             </Switch>
-        </Router>
+          )} />
+        </HashRouter>
     );
   }
 }
